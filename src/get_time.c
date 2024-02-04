c4e015b9ec3b856277e30ea27ed6ed444c055bcc
Leon David Zipp <leondavidzipp@gmx.de>
2024-02-04 15:02:38 +0100

    fixed alloc error

--- src/get_time.c
+++ src/get_time.c
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 14:58:44 by lzipp             #+#    #+#             */
-/*   Updated: 2024/02/04 14:59:05 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/02/04 15:02:26 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ long long	get_time(void)
 	struct timeval		*tv;
 	long long			ms;
 
+	tv = (struct timeval *)malloc(sizeof(struct timeval));
 	gettimeofday(tv, NULL);
 	ms = tv->tv_sec * 1000LL + tv->tv_usec / 1000LL;
 	free(tv);
