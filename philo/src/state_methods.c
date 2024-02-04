9b030379e954d4ca8ea699145bb9c2d72c6c1e69
Leon David Zipp <leondavidzipp@gmx.de>
2024-02-04 22:02:20 +0100

    rough draft of methods

--- philo/src/state_methods.c
+++ philo/src/state_methods.c
@@ -6,16 +6,32 @@
 /*   By: lzipp <lzipp@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 21:17:28 by lzipp             #+#    #+#             */
-/*   Updated: 2024/02/04 21:18:50 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/02/04 22:01:48 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
-void 	take_forks(t_philo *philo)
+void	philo_think(t_philo *philo)
 {
-	pthread_mutex_lock(&philo->left_fork);
-	fork_message(get_time() - philo->data->start_time, philo->id);
-	pthread_mutex_lock(&philo->right_fork);
-	fork_message(get_time() - philo->data->start_time, philo->id);
+	think_message(get_time(), philo->id);
+	pthread_mutex_lock(mutex);
 }
+
+void	philo_eat(t_philo *philo, pthread_mutex_t *mutex)
+{
+	eat_message(get_time(), philo->id);
+	usleep(philo->ms_to_eat);
+}
+
+void	philo_sleep(t_philo *philo)
+{
+	sleep_message(get_time(), philo->id);
+	usleep(philo->ms_to_sleep);
+}
+
+void	philo_death(void)
+{
+	death_message(get_time(), id);
+}
+
