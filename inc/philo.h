3c6fb5119aecf88808194a00a8fd30b9661fef9e
Leon David Zipp <leondavidzipp@gmx.de>
2024-02-04 12:42:18 +0100

    finished parsing, renamed t_to to ms_to

--- inc/philo.h
+++ inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 12:02:49 by lzipp             #+#    #+#             */
-/*   Updated: 2024/02/04 12:26:47 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/02/04 12:41:11 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,19 @@
 typedef struct s_data
 {
 	int		philo_cnt;
-	int		t_to_die;
-	int		t_to_eat;
-	int		t_to_sleep;
+	int		fork_cnt;
+	int		ms_to_die;
+	int		ms_to_eat;
+	int		ms_to_sleep;
 	int		must_eat_cnt;
 }				t_data;
 
 typedef struct s_philo
 {
 	int		id;
-	int		t_to_die;
-	int		t_to_eat;
-	int		t_to_sleep;
+	int		ms_to_die;
+	int		ms_to_eat;
+	int		ms_to_sleep;
 	int		must_eat_cnt;
 }				t_philo;
 
