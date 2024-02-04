2c4247f208e1616f29ec162d46b5529499ff3b95
Leon David Zipp <leondavidzipp@gmx.de>
2024-02-04 14:17:56 +0100

    added messages to .h, also parse_input function

--- src/messages.c
+++ src/messages.c
@@ -6,16 +6,10 @@
 /*   By: lzipp <lzipp@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 13:59:31 by lzipp             #+#    #+#             */
-/*   Updated: 2024/02/04 14:14:39 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/02/04 14:15:54 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-// timestamp_in_ms X has taken a fork
-// ◦ timestamp_in_ms X is eating
-// ◦ timestamp_in_ms X is sleeping
-// ◦ timestamp_in_ms X is thinking
-// ◦ timestamp_in_ms X died
 #include "../inc/philo.h"
 
 void	fork_message(long long ms, int id)
