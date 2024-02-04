5f5a450d613f6ce16f61a27657cdc40e680b1cbe
Leon David Zipp <leondavidzipp@gmx.de>
2024-02-04 15:06:49 +0100

    fixed norm error in .h

--- inc/philo.h
+++ inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 12:02:49 by lzipp             #+#    #+#             */
-/*   Updated: 2024/02/04 14:59:30 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/02/04 15:06:35 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,16 +42,16 @@ typedef struct s_philo
 }				t_philo;
 
 // input_parsing
-t_data	*parse_input(int argc, char **argv);
+t_data		*parse_input(int argc, char **argv);
 
 // time
 long long	get_time(void);
 
 // messages
-void	fork_message(long long ms, int id);
-void	eat_message(long long ms, int id);
-void	sleep_message(long long ms, int id);
-void	think_message(long long ms, int id);
-void	death_message(long long ms, int id);
+void		fork_message(long long ms, int id);
+void		eat_message(long long ms, int id);
+void		sleep_message(long long ms, int id);
+void		think_message(long long ms, int id);
+void		death_message(long long ms, int id);
 
 #endif
