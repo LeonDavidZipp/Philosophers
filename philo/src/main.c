b710fd1983fe533a0c3ccc261cc166e633fe25db
LeondavidZipp <leondavidzipp@gmx.de>
2024-02-05 11:26:12 +0100

    nothing special

--- philo/src/main.c
+++ philo/src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lzipp <lzipp@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 12:48:08 by lzipp             #+#    #+#             */
-/*   Updated: 2024/02/04 22:23:09 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/02/05 11:26:04 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,14 @@ int	main(int argc, char **argv)
 {
 	t_data				*data;
 	t_data				**philos;
-	pthread_mutex_t		*mutex;
+	pthread_mutex_t		*print_mutex;
 
 	data = create_data(argc, argv);
-	mutex = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
-	mutex = init_mutex(data);
+	print_mutex = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
+	pthread_mutex_init(print_mutex, NULL);
 	philos = init_philos(data);
 	start_simulation(data);
 	cleanup(data);
+	pthread_mutex_destroy(print_mutex);
 	return (0);
 }
