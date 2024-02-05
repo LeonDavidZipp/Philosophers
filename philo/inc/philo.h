cc44aae07d8fcd58d55f7f37948c3ffb63b00acc
LeondavidZipp <leondavidzipp@gmx.de>
2024-02-05 18:58:18 +0100

    mutex protected print statements

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 12:02:49 by lzipp             #+#    #+#             */
-/*   Updated: 2024/02/05 18:54:37 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/02/05 18:58:13 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ pthread_mutex_t	**create_forks(t_data *data);
 t_philo			**create_philos(t_data *data, pthread_mutex_t **forks);
 
 // philo_routine
-void			philo_routine(t_philo *philo);
+void			philo_routine(t_philo *philo, pthread_mutex_t *p_mut);
 
 // time
 long long		get_time(void);
