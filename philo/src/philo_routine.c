d8c3732b5899f9dc0738fcf4841b548800433457
LeondavidZipp <leondavidzipp@gmx.de>
2024-02-06 22:37:11 +0100

    adjsuted philo_routine

--- philo/src/philo_routine.c
+++ philo/src/philo_routine.c
@@ -6,15 +6,15 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 21:17:28 by lzipp             #+#    #+#             */
-/*   Updated: 2024/02/06 21:59:19 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/02/06 22:36:56 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
-static bool	check_alive(t_routine *r, long long time);
-static bool	philo_eat(t_routine *r);
-static bool	philo_sleep(t_routine *r);
+static void	check_alive(t_routine *r, long long time);
+static void	philo_eat(t_routine *r);
+static void	philo_sleep(t_routine *r);
 static void	philo_death(t_routine *r);
 
 bool	philo_routine(t_routine *r)
@@ -22,23 +22,25 @@ bool	philo_routine(t_routine *r)
 	bool	is_alive;
 
 	is_alive = true;
-	while (r->philo->must_eat_cnt == -1
-		|| r->philo->must_eat_cnt > 0)
+	while ((r->philo->must_eat_cnt == -1
+			|| r->philo->must_eat_cnt > 0) && *r->some_died == false)
 	{
-		think_message(get_time(), r->philo->id, r->p_mut);
-		is_alive = philo_eat(r);
-		if (!is_alive)
+		check_alive(r, -1);
+		if (*r->some_died)
 			break ;
-		is_alive = philo_sleep(r);
-		if (!is_alive)
+		think_message(get_time(), r->philo->id, r->p_mut);
+		philo_eat(r);
+		if (*r->some_died)
+			break ;
+		philo_sleep(r);
+		if (*r->some_died)
 			break ;
 		if (r->philo->must_eat_cnt > 0)
 			r->philo->must_eat_cnt--;
 	}
-	r->some_died = !is_alive;
 }
 
-static bool	philo_eat(t_routine *r)
+static void	philo_eat(t_routine *r)
 {
 	long long	ms_new_ate_at;
 
@@ -54,15 +56,14 @@ static bool	philo_eat(t_routine *r)
 	}
 	ms_new_ate_at = get_time();
 	check_alive(r, ms_new_ate_at);
-	r->philo->ms_last_ate_at = ms_new_ate_at;
 	eat_message(r->philo->ms_last_ate_at, r->philo->id, r->p_mut);
+	r->philo->ms_last_ate_at = ms_new_ate_at + r->philo->ms_to_eat;
 	usleep(r->philo->ms_to_eat);
 	pthread_mutex_unlock(r->philo->left_fork);
 	pthread_mutex_unlock(r->philo->right_fork);
-	return (true);
 }
 
-static bool	philo_sleep(t_routine *r)
+static void	philo_sleep(t_routine *r)
 {
 	sleep_message(get_time(), r->philo->id, r->p_mut);
 	usleep(r->philo->ms_to_sleep);
@@ -77,16 +78,12 @@ static void	philo_death(t_routine *r)
 	pthread_mutex_unlock(r->death_mut);
 }
 
-static bool	check_alive(t_routine *r, long long time)
+static void	check_alive(t_routine *r, long long time)
 {
 	if (time == -1)
 		time = get_time();
 	if (time - r->philo->ms_last_ate_at > r->philo->ms_to_die)
-	{
 		philo_death(r);
-		return (false);
-	}
 	if (time > 0)
 		r->philo->ms_last_ate_at = time;
-	return (true);
 }
