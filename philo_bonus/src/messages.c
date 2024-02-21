04632c5555d56328eec4f6c364d8ee442135126f
LeondavidZipp <leondavidzipp@gmx.de>
2024-02-21 18:31:25 +0100

    partially removed mutexes

--- philo_bonus/src/messages.c
+++ philo_bonus/src/messages.c
@@ -6,37 +6,37 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 13:59:31 by lzipp             #+#    #+#             */
-/*   Updated: 2024/02/21 18:29:37 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/02/21 18:31:20 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
-void	fork_message(long long ms, t_p_routine *r)
+void	fork_message(long long ms, int id)
 {
 	printf("\033[0;36m");
-	printf("%lld %d has taken a fork", ms, r->philo->id);
+	printf("%lld %d has taken a fork", ms, id);
 	printf("\033[0m\n");
 }
 
-void	eat_message(long long ms, t_p_routine *r)
+void	eat_message(long long ms, int id)
 {
 	printf("\033[0;32m");
-	printf("%lld %d is eating", ms, r->philo->id);
+	printf("%lld %d is eating", ms, id);
 	printf("\033[0m\n");
 }
 
-void	sleep_message(long long ms, t_p_routine *r)
+void	sleep_message(long long ms, int id)
 {
 	printf("\033[0;35m");
-	printf("%lld %d is sleeping", ms, r->philo->id);
+	printf("%lld %d is sleeping", ms, id);
 	printf("\033[0m\n");
 }
 
-void	think_message(long long ms, t_p_routine *r)
+void	think_message(long long ms, int id)
 {
 	printf("\033[0;33m");
-	printf("%lld %d is thinking", ms, r->philo->id);
+	printf("%lld %d is thinking", ms, id);
 	printf("\033[0m\n");
 }
 
