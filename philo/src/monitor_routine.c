6128ad8068caf2d4d5241c155861ea05287d4e31
LeondavidZipp <leondavidzipp@gmx.de>
2024-03-27 11:45:44 +0100

    removed clutter, fixed last error

--- philo/src/monitor_routine.c
+++ philo/src/monitor_routine.c
@@ -6,25 +6,29 @@
 /*   By: lzipp <lzipp@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 17:44:33 by lzipp             #+#    #+#             */
-/*   Updated: 2024/02/21 13:04:11 by lzipp            ###   ########.fr       */
+/*   Updated: 2024/03/27 10:39:41 by lzipp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
 static bool		monitor_death(t_m_routine *r);
-static bool		monitor_all_ate(t_m_routine *r);
+static bool		monitor_all_ate(t_m_routine *r, int initial_must_eat_count);
 
 void	*monitor_routine(void *r_void)
 {
 	t_m_routine	*r;
+	int			initial_must_eat_count;
 
 	r = (t_m_routine *)r_void;
+	pthread_mutex_lock(r->philos[0]->eat_mut);
+	initial_must_eat_count = r->philos[0]->must_eat_cnt;
+	pthread_mutex_unlock(r->philos[0]->eat_mut);
 	while (true)
 	{
 		if (!monitor_death(r))
 			return (NULL);
-		if (monitor_all_ate(r))
+		if (monitor_all_ate(r, initial_must_eat_count))
 			return (NULL);
 	}
 	return (NULL);
@@ -55,11 +59,11 @@ static bool	monitor_death(t_m_routine *r)
 	return (true);
 }
 
-static bool	monitor_all_ate(t_m_routine *r)
+static bool	monitor_all_ate(t_m_routine *r, int initial_must_eat_count)
 {
 	int	i;
 
-	if (r->philos[0]->must_eat_cnt == -1)
+	if (initial_must_eat_count == -1)
 		return (false);
 	i = -1;
 	while (++i < r->philo_cnt)
